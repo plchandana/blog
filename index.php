ff62402603eb28fd6a53599189099f0d759ce1f7
chandana <plchandanacgr@gmail.com>
2022-10-18 21:55:58 +0530

    i created my block

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="plugin/bootstrap.min.css">
+    <script src="plugin/jquery.min.js"></script>
+    <script src="plugin/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+    <title>my blog</title>
+</head>
+<body>
+    <?php include_once("inc/navbar.php")?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="jumbotron mt-4">
+                   <h2 id="jumbo-header"> welcome to the Dev tube</h2>
+                   <h2 id="jumbo-emoji"> 👨‍💻👨‍🎓 👨‍💻</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
